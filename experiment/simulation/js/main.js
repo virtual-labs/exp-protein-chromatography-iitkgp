a72e870187d2f58641a57af14582dcae3508777c
prakriti5dhang <prakritidhang@gmail.com>
2023-11-03 18:03:20 +0530

    exp6

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -2,3 +2,95 @@
 Exp name: Protein purification by affinity chromatography
 File name: main.js
 Developer: Prakriti Dhang */
+
+function start(){
+
+}
+
+
+function restartexp(){
+
+}
+
+
+function showcolumn(){
+    document.getElementById("emptycolumn").style.display="block";
+}
+
+function showeq(){
+
+}
+
+
+function cellysate(){
+
+}
+
+function prointerest(){
+
+}
+
+function sampleload(){
+
+}
+
+function washing(){
+
+}
+
+function buffera(){
+
+}
+
+
+function removeunpro(){
+
+}
+
+
+function bindingpro(){
+
+}
+
+ /* Elution starts*/ 
+
+ function elu10(){
+
+ }
+
+ function elu20(){
+    
+ }
+ function elu40(){
+    
+ }
+
+ function elu80(){
+    
+ }
+ function elu100(){
+    
+ }
+
+/**  Data Acquistion*/
+
+
+
+function dataacq(){
+    document.getElementById("graph").style.display="block";
+}
+
+
+
+function sdspage(){
+document.getElementById("stepgel").style.display="block";
+}
+
+
+
+
+function showgel(){
+    window.scrollBy(0,2500);
+    
+    document.getElementById("optgel").style.display="block";
+}
